4eab27b71ae5bbdc99a478bef516e6d1a28549e0
KattaChad <152790720+KattaChad@users.noreply.github.com>
2025-09-17 00:57:07 +0530

    Added controller

---
+++ order-service/src/main/java/com/example/order_service/controller/OrderController.java
@@ -0,0 +1,31 @@
+package com.example.order_service.controller;
+
+import com.example.order_service.dto.OrderRequest;
+import com.example.order_service.service.OrderService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<OrderRequest>> getAllOrders() {
+        List<OrderRequest> orders = orderService.getAllOrders();
+        return ResponseEntity.ok(orders);
+    }
+
+    @PostMapping
+    public ResponseEntity<OrderRequest> placeOrder(@RequestBody OrderRequest orderRequest) {
+        OrderRequest placedOrder = orderService.placeOrder(orderRequest);
+        return ResponseEntity.ok(placedOrder);
+    }
+}
